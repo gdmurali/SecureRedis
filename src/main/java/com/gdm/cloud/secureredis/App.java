a3871705d6b3f543f778780dfcef53c7268f2589
Murali G D <murali@versa-networks.com>
2017-07-04 18:38:45 +0530

    first commit

---
+++ src/main/java/com/gdm/cloud/secureredis/App.java
@@ -0,0 +1,13 @@
+package com.gdm.cloud.secureredis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
